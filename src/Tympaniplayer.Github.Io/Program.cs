d3a430c9655a95312d7704c950a25c3bcb04708c
Nate Palmer <nate@natedpalm.com>
2021-03-17 12:26:16 -0400

    WIP, add blazorise

--- src/Tympaniplayer.Github.Io/Program.cs
+++ src/Tympaniplayer.Github.Io/Program.cs
@@ -3,6 +3,9 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using Blazorise;
+using Blazorise.Bulma;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +18,13 @@ namespace Tympaniplayer.Github.Io
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services
+              .AddBlazorise(options =>
+              {
+                options.ChangeTextOnKeyPress = true;
+              })
+              .AddBulmaProviders()
+              .AddFontAwesomeIcons();
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(
